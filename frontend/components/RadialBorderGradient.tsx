3d8678d3d4346aae3b6ee13be1807857d9a74e03
samuel_ndjouli <samuelndj.997@gmail.com>
2024-02-29 00:15:35 +0100

    modified the 'bottom' className

--- frontend/components/RadialBorderGradient.tsx
+++ frontend/components/RadialBorderGradient.tsx
@@ -6,7 +6,7 @@ interface RadialBorderGradientProps {
 
 const RadialBorderGradient = ({direction}: RadialBorderGradientProps) => {
   return (
-    <div className={`absolute inset-x-0 h-px w-1/4 mx-auto -${direction}-px shadow-2xl  bg-gradient-to-r from-transparent via-[#B4B4B4]/70 to-transparent`} />
+    <div className={`absolute inset-x-0 h-px w-1/4 mx-auto ${direction}-0 shadow-2xl  bg-gradient-to-r from-transparent via-[#B4B4B4]/70 to-transparent`} />
   )
 }
 
