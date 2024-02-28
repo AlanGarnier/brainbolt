2ec88d9345ce4d89fa55ed558b0a81160059fc07
samuel_ndjouli <samuelndj.997@gmail.com>
2024-02-28 23:46:52 +0100

    getting the code more readable

--- frontend/components/homepage/sections/hero/Hero.tsx
+++ frontend/components/homepage/sections/hero/Hero.tsx
@@ -14,8 +14,12 @@ const Hero = () => {
       <div className="flex flex-col mx-auto lg:max-w-[796px] space-y-[40px] items-center justify-center h-full text-center">
         <h1 className="hero-title uppercase">Lorem ipsum dolor sit amet consectetur</h1>
         <p className="hero-subtitle">Lorem ipsum dolor sit amet consectetur. Euismod nam in ut nulla nulla euismod. Proin fermentum mi vitae diam id. Purus est urna lectus malesuada et gravida porttitor fames. Nullam aliquet aliquam eget malesuada id sed rhoncus. Est metus elementum odio felis facilisi non natoque cras vitae.  </p>
-        <Button asChild className="glow">
-          <Link className="font-semibold font-jost text-[16px]" href="/login">
+        <Button 
+          asChild 
+          className="glow">
+          <Link 
+            href="/login"
+            className="font-semibold font-jost text-base">
             Lancez la partie
           </Link>
         </Button>
