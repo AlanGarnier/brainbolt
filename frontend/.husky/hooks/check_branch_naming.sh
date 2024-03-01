423223b7399b1270b8cc528ea0af1fab8ce9c091
samuel_ndjouli <samuelndj.997@gmail.com>
2024-03-01 15:35:21 +0100

    modified the error message

--- frontend/.husky/hooks/check_branch_naming.sh
+++ frontend/.husky/hooks/check_branch_naming.sh
@@ -4,7 +4,7 @@
 
 local_branch_name="$(git rev-parse --abbrev-ref HEAD)"
 valid_branch_regex='^(feature|fix|refacto)(\(back\)|\(front\)|\(devops\)|\(git\)|\(k8s\)|\(docker\)|\(deploy\)|\(aws\))/[a-zA-Z0-9\-]+$'
-message="There is something wrong with your branch name. Branch names in this project must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
+message="Yo 👋 !\nLe nom de la branche que essayes de push ne respecte pas le format qu'on a instauré ❌\nLe schéma attendu est le suivant : $valid_branch_regex.\n(exemple: feature(front)/create-button).\nTu peux modifier facilement le nom de ta branche avec : git branch -m <nouveau-nom>, puis push à nouveau !"
 
 if [[ ! $local_branch_name =~ $valid_branch_regex ]]; then
   echo "$message"
