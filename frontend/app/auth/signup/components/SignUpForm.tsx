23e8ba1f81e45947f890569c79a12f06efdbf50e
samuel_ndjouli <samuelndj.997@gmail.com>
2024-03-02 10:08:36 +0100

    created SignUpForm component

---
+++ frontend/app/auth/signup/components/SignUpForm.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from 'react';
+import Steps from './Steps';
+
+const SignUpForm = () => {
+  return (
+    <>
+        <Steps />
+        <form className="space-y-8 w-full">
+
+        </form>
+    </>
+  )
+}
+
+export default SignUpForm
