f3d26f514a2c35eae4fe6eff25052e0080244698
Samuel NDJOULI AMOUGOU <samuel@kelbongoo.com>
2024-03-04 15:56:38 +0100

    removed husky files

--- frontend/.husky/hooks/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Change directory to the hooks folder
-cd "$(dirname "$0")"
-
-# Grant execute permissions to the check_branch_naming.sh script
-chmod +x check_branch_naming.sh
-
-echo "Setup complete!"
