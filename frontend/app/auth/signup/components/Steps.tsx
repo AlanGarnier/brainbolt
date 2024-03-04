b4b45f2dfdff99ac8d194e36a153ea4692c5839c
samuel_ndjouli <samuelndj.997@gmail.com>
2024-03-04 22:40:38 +0100

    created steps component

---
+++ frontend/app/auth/signup/components/Steps.tsx
@@ -0,0 +1,45 @@
+"use client";
+// import React, { useState } from 'react';
+
+interface StepsProps {
+    steps: {
+        id: string,
+        name: string,
+        fields: string[]
+    }[],
+    currentStep: number
+}
+
+const Steps = ({steps, currentStep}: StepsProps) => {
+
+  return (
+    <nav className="mb-12">
+        <ol className="items-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0 rtl:space-x-reverse">
+            {
+                steps.map((step, index) => (
+                    <li key={step.name} className="flex items-center font-ubuntu font-bold space-x-2.5 rtl:space-x-reverse">
+                    {currentStep > index ? (
+                        <>
+                            <span className="flex items-center justify-center w-8 h-8 border border-white/60 rounded-full shrink-0 text-base">{step.id}</span>
+                            <h3 className="font-medium leading-tight text-white/60">{step.name}</h3>
+                        </>
+                    ) : currentStep === index ? (
+                        <>
+                            <span className="flex items-center justify-center w-8 h-8 border border-white bg-white rounded-full shrink-0 text-base text-primary-black">{step.id}</span>
+                            <h3 className="font-medium leading-tight text-white">{step.name}</h3>
+                        </>
+                    ) : (
+                        <>
+                        <span className="flex items-center justify-center w-8 h-8 border border-white/60 rounded-full shrink-0 text-base">{step.id}</span>
+                        <h3 className="font-medium leading-tight text-white/60">{step.name}</h3>
+                        </>
+                    )}
+                  </li>
+                ))
+            }
+        </ol>
+    </nav>
+  )
+}
+
+export default Steps
