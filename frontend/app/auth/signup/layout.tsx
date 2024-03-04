5bc16376537c8985df4fa9c45e16187fdf542417
samuel_ndjouli <samuelndj.997@gmail.com>
2024-03-04 22:38:37 +0100

    indented code

--- frontend/app/auth/signup/layout.tsx
+++ frontend/app/auth/signup/layout.tsx
@@ -8,13 +8,17 @@ interface LayoutProps {
 const Layout = ({children}: LayoutProps) => {
   return (
     <>
-        <section className="w-full flex">
+        <section className="w-full h-screen flex">
           <div className="bg-signup pb-16 hidden lg:block lg:py-48 lg:w-1/3">
             &nbsp;
           </div>
-          <div className="pt-28 pb-16 px-8 lg:px-[140px] lg:py-32 w-full lg:w-2/3 flex flex-col justify-center">
+          <div className="px-8 py-8 lg:px-[140px] lg:pt-12 lg:pb-28 w-full lg:w-2/3 flex flex-col">
             <div className="flex justify-end pb-10 lg:pb-[72px]">
-              <Link href="/" className="font-ubuntu font-bold text-[12px] text-[#E9E9E9]">Retour à l&apos;accueil</Link>
+              <Link 
+                href="/" 
+                className="font-ubuntu font-bold text-[12px] text-[#E9E9E9]"
+                >Retour à l&apos;accueil
+              </Link>
             </div>
             {children}
           </div>
