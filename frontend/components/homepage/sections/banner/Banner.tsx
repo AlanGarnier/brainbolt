5c0c538e81c1b3f4f1ca289624f10bdd371d98aa
malika <malikachoubri@gmail.com>
2024-03-05 21:39:47 +0100

    update page 'add banner component ' and creat banner component

--- frontend/components/homepage/sections/banner/Banner.tsx
+++ frontend/components/homepage/sections/banner/Banner.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Section from '@/components/Section'
+
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <Section>
+      <div className="flex flex-col mx-auto lg:max-w-[796px] space-y-[40px] items-center justify-center h-full text-center">
+        <h1 className="banner-title">Lorem ipsum dolor sit amet consectetur. </h1>
+        <h2 className="banner-text">Lorem ipsum dolor sit amet consectetur. Euismod nam in ut nulla nulla euismod.</h2>
+
+        <div className="inline">
+          <Button
+            asChild
+            variant={'default'}>
+            <Link
+              href="/login"
+              className="font-semibold font-jost text-[16px]">
+              Lancez la partie
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </Section>
   )
 }
 
-export default Banner
+export default Banner;
