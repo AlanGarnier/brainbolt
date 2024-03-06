33f0214095a887e0bd5518b1c5078c222fa26cd3
samuel_ndjouli <samuelndj.997@gmail.com>
2024-03-06 19:16:41 +0100

    added a new optionnal prop in Section component

--- frontend/components/Section.tsx
+++ frontend/components/Section.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 interface SectionProps {
   children: React.ReactNode;
+  background?: string;
 }
 
-const Section = ({children}: SectionProps) => {
+const Section = ({children, background}: SectionProps) => {
   return (
-    <section className="container mx-auto px-8 pt-5 pb-8 lg:pt-[90px] lg:pb-[110px] lg:px-[110px] bg-section ">
+    <section className={`${background} container mx-auto px-8 pt-5 pb-8 lg:pt-[90px] lg:pb-[110px] lg:px-[110px]`}>
       {children}
     </section>
   )
