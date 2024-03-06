4a062ff1e967ebef96e26a893e704c8a1879cf2a
samuel_ndjouli <samuelndj.997@gmail.com>
2024-03-06 22:22:08 +0100

    made some modifications - added radial gradient

--- frontend/components/homepage/sections/footer/Footer.tsx
+++ frontend/components/homepage/sections/footer/Footer.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { NavLink } from '@/components/CustomTexts';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button'
 
 import Link from 'next/link';
+import RadialBorderGradient from '@/components/RadialBorderGradient';
 
 const Footer = () => {
 
@@ -11,36 +11,35 @@ const Footer = () => {
   return (
     <>
 
-      <footer className="relative px-8 pt-5 pb-8 lg:py-4 lg:px-[110px]">
-
-        <div className="flex justify-between h-16 mx-auto">
-          {/* Logo */}
-          <div>
-            <Link href="/" className="flex items-center">
-              <Image src="/assets/img/brainbolt-white-logo.svg" alt="Brainbolt" width={194} height={30} />
-            </Link>
-          </div>
-          {/* Texte */}
-          <ul className="items-stretch hidden space-x-3 lg:flex">
-            <NavLink link="/">Plan su site</NavLink>
-            <NavLink link="/">Plan su site</NavLink>
-            <NavLink link="/">Plan su site</NavLink>
-          </ul>
-
-          {/* Bouton */}
-          <div className="items-center flex-shrink-0 hidden lg:flex lg:space-x-5">
-            <Button asChild variant={'default'} className="glow">
-              <Link href="/login" className="font-semibold font-jost">
-                Se connecter
+      <footer className="relative px-8 pt-5 pb-8 lg:pt-10 lg:pb-8 lg:px-[110px]">
+        <div className="mx-auto pb-2 lg:pb-20 flex flex-col lg:flex-row justify-between items-center">
+            {/* Logo */}
+            <div className="flex pb-10 lg:pb-0 justify-center items-center">
+              <Link href="/">
+                <Image src="/assets/img/brainbolt-white-logo.svg" alt="Brainbolt" width={194} height={30} />
               </Link>
-            </Button>
+            </div>
+            {/* Texte */}
+            <ul className="flex flex-col lg:flex-row justify-center items-center lg:space-x-7">
+              <li>Plan du site</li>
+              <li>Plan du site</li>
+              <li>Plan du site</li>
+            </ul>
+            {/* Bouton */}
+            <div className="items-center flex-shrink-0 hidden lg:flex lg:space-x-5">
+              <Button asChild variant={'default'} className="glow">
+                <Link href="/login" className="font-semibold font-jost">
+                  Se connecter
+                </Link>
+              </Button>
+            </div>
           </div>
-          {/* Bas de page */}
+          {/* Radial gradient at the top of the footer */}
+          <RadialBorderGradient direction="top" />
+          <div className="py-8 lg:py-4 text-sm text-gray-400 flex justify-center lg:justify-center">
+            © BrainBolt - Tous les droits préservés
+          </div>
 
-        </div>
-        <div className="mt-16 text-sm text-gray-400 flex justify-center lg:justify-center">
-          © BrainBolt - Tous les droits préservés
-        </div>
       </footer>
     </>
   )
