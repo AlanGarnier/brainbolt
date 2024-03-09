005c6b641ade4a45e69d9c53785638e83c18b42f
zoh-ra <raitonoums@gmail.com>
2024-03-09 21:30:13 +0100

    creation section map

--- frontend/components/homepage/sections/map/Map.tsx
+++ frontend/components/homepage/sections/map/Map.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Section from '@/components/Section'
+import Image from 'next/image'
+import { MapSubtitle, MapTitle } from '@/components/CustomTexts'
 
 const Map = () => {
   return (
-    <div>Map</div>
-  )
-}
+    <Section>
+      <div className="flex flex-col items-center justify-center space-y-8">
+        <MapSubtitle>Lorem ipsum</MapSubtitle>
+        <MapTitle>Lorem ipsum dolor sit amet consectetur. Euismod nam in ut nulla nulla euismod. Proin fermentum mi vitae diam id.</MapTitle>
+        <Image src="/assets/img/map.png" alt="Map" width={1600} height={3200} />
+      </div>
+    </Section>
+  );
+};
 
-export default Map
+export default Map;
