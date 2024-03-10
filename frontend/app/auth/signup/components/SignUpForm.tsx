ab4f41fbc18a23d65e9a8150c602d4f51910e031
samuel_ndjouli <samuelndj.997@gmail.com>
2024-03-10 09:45:16 +0100

    modified steps, added ternary conditions based on currentStep value

--- frontend/app/auth/signup/components/SignUpForm.tsx
+++ frontend/app/auth/signup/components/SignUpForm.tsx
@@ -2,35 +2,36 @@
 import React, { useState } from 'react';
 import Steps from './Steps';
 import Input from '@/components/forms/Input';
+import { SectionTitle } from '@/components/CustomTexts';
 
 const steps = [
   {
     id: '1',
-    name: 'Personal Information',
-    fields: ['firstName', 'lastName', 'email']
+    name: 'Email/Mot de passe',
+    fields: ['email', 'password']
   },
   {
     id: '2',
-    name: 'Address',
-    fields: ['country', 'state', 'city', 'street', 'zip']
+    name: 'Nom d’utilisateur',
+    fields: ['username', 'state', 'city', 'street', 'zip']
   },
-  { id: '3', name: 'Complete', fields: ['toto'] }
+  { id: '3', name: 'Avatar', fields: ['image'] }
 ]
 
 const SignUpForm = () => {
-  // const [previousStep, setPreviousStep] = useState(0)
+  const [previousStep, setPreviousStep] = useState(0)
   const [currentStep, setCurrentStep] = useState(0)
   // const delta = currentStep - previousStep
 
   const next = () => {
     // const fields = steps[currentStep].fields
-      // setPreviousStep(currentStep)
+      setPreviousStep(currentStep)
       setCurrentStep(step => step + 1)
     }
 
   const prev = () => {
     if (currentStep > 0) {
-      // setPreviousStep(currentStep)
+      setPreviousStep(currentStep)
       setCurrentStep(step => step - 1)
     }
   }
@@ -44,30 +45,52 @@ const SignUpForm = () => {
         />
         {/* Form */}
         <form className="space-y-8 w-full">
-          <Input
-            id="name"
-            type="text"
-            // label="Nom"
-            placeholder="Nom d'utilisateur"
-            // register={register("name")}
-            // error={errors.name?.message}
-          />
-          <Input
-            id="email"
-            type="email"
-            // label="Email"
-            placeholder="Email"
-            // register={register("email")}
-            // error={errors.email?.message}     
-          />
-          <Input
-            id="password"
-            type="password"
-            // label="Mot de passe"
-            placeholder="Mot de passe"
-            // register={register("password")}
-            // error={errors.password?.message}
-          />
+          { currentStep === 0 && (
+            <div>
+              <SectionTitle 
+                className='bg-clip-text text-transparent mb-8 bg-gradient-to-br from-white to-[#52525B]'>
+                Commençons par ton email/mot de passe
+              </SectionTitle>
+              <Input
+                id="email"
+                type="email"
+                label="Email"
+                placeholder="Email"
+                // register={register("email")}
+                // error={errors.email?.message}     
+              />
+              <Input
+                id="password"
+                type="password"
+                label="Mot de passe"
+                placeholder="Mot de passe"
+                // register={register("password")}
+                // error={errors.password?.message}
+              />
+            </div>
+          )}
+          { currentStep === 1 && (
+            <div>
+              <SectionTitle 
+                className='bg-clip-text text-transparent mb-8 bg-gradient-to-br from-white to-[#52525B]'>
+                Trouve-toi un nom de joueur unique !
+              </SectionTitle>
+              <Input
+                id="name"
+                type="text"
+                label="Nom"
+                placeholder="Nom d'utilisateur"
+                // register={register("name")}
+                // error={errors.name?.message}
+              />
+            </div>
+          )}
+          { currentStep === 2 && (
+            <div>
+              <h2 className="text-2xl font-bold text-white">Complete</h2>
+              <p className="text-white/60">Youre all set</p>
+            </div>
+          )}
         </form>
         {/* Navigation */}
         <div className='mt-8 pt-5'>
