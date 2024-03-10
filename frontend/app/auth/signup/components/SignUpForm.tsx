4ec9af651c69e29a09c3e8604998c2519bed732d
samuel_ndjouli <samuelndj.997@gmail.com>
2024-03-10 11:20:40 +0100

    removed the labels, added min-height to form, changed the navigations button

--- frontend/app/auth/signup/components/SignUpForm.tsx
+++ frontend/app/auth/signup/components/SignUpForm.tsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import Steps from './Steps';
 import Input from '@/components/forms/Input';
 import { SectionTitle } from '@/components/CustomTexts';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 const steps = [
   {
@@ -19,19 +21,19 @@ const steps = [
 ]
 
 const SignUpForm = () => {
-  const [previousStep, setPreviousStep] = useState(0)
+  // const [previousStep, setPreviousStep] = useState(0)
   const [currentStep, setCurrentStep] = useState(0)
   // const delta = currentStep - previousStep
 
   const next = () => {
     // const fields = steps[currentStep].fields
-      setPreviousStep(currentStep)
+      // setPreviousStep(currentStep)
       setCurrentStep(step => step + 1)
     }
 
   const prev = () => {
     if (currentStep > 0) {
-      setPreviousStep(currentStep)
+      // setPreviousStep(currentStep)
       setCurrentStep(step => step - 1)
     }
   }
@@ -44,7 +46,7 @@ const SignUpForm = () => {
           currentStep={currentStep} 
         />
         {/* Form */}
-        <form className="space-y-8 w-full">
+        <form className="space-y-8 w-full min-h-[295px]">
           { currentStep === 0 && (
             <div>
               <SectionTitle 
@@ -54,7 +56,6 @@ const SignUpForm = () => {
               <Input
                 id="email"
                 type="email"
-                label="Email"
                 placeholder="Email"
                 // register={register("email")}
                 // error={errors.email?.message}     
@@ -62,7 +63,6 @@ const SignUpForm = () => {
               <Input
                 id="password"
                 type="password"
-                label="Mot de passe"
                 placeholder="Mot de passe"
                 // register={register("password")}
                 // error={errors.password?.message}
@@ -78,7 +78,6 @@ const SignUpForm = () => {
               <Input
                 id="name"
                 type="text"
-                label="Nom"
                 placeholder="Nom d'utilisateur"
                 // register={register("name")}
                 // error={errors.name?.message}
@@ -87,56 +86,37 @@ const SignUpForm = () => {
           )}
           { currentStep === 2 && (
             <div>
-              <h2 className="text-2xl font-bold text-white">Complete</h2>
-              <p className="text-white/60">Youre all set</p>
+              <SectionTitle 
+                className='bg-clip-text text-transparent mb-8 bg-gradient-to-br from-white to-[#52525B]'>
+                Presque fini ! Pimp ton profil en ajoutant une image d&apos;avatar !
+              </SectionTitle>
+              <Input
+                id="name"
+                type="text"
+                placeholder="Nom d'utilisateur"
+                // register={register("name")}
+                // error={errors.name?.message}
+              />
             </div>
           )}
         </form>
         {/* Navigation */}
         <div className='mt-8 pt-5'>
           <div className='flex justify-between'>
-            <button
-              type='button'
+            <Button
               onClick={prev}
               disabled={currentStep === 0}
-              className='rounded bg-white px-2 py-1 text-sm font-semibold text-sky-900 shadow-sm ring-1 ring-inset ring-sky-300 hover:bg-sky-50 disabled:cursor-not-allowed disabled:opacity-50'
+              variant='navigation'
             >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='1.5'
-                stroke='currentColor'
-                className='h-6 w-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M15.75 19.5L8.25 12l7.5-7.5'
-                />
-              </svg>
-            </button>
-            <button
-              type='button'
+              <ChevronLeft className='h-5 w-5' />
+            </Button>
+            <Button
               onClick={next}
               disabled={currentStep === steps.length - 1}
-              className='rounded bg-white px-2 py-1 text-sm font-semibold text-sky-900 shadow-sm ring-1 ring-inset ring-sky-300 hover:bg-sky-50 disabled:cursor-not-allowed disabled:opacity-50'
+              variant='navigation'
             >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='1.5'
-                stroke='currentColor'
-                className='h-6 w-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M8.25 4.5l7.5 7.5-7.5 7.5'
-                />
-              </svg>
-            </button>
+              <ChevronRight className='h-5 w-5' />
+            </Button>
           </div>
         </div>
     </>
