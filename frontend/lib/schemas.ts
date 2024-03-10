a9f8896676ff2c77df6a98a5af64ef6bb2c268c1
samuel_ndjouli <samuelndj.997@gmail.com>
2024-03-10 11:39:10 +0100

    added error messages

--- frontend/lib/schemas.ts
+++ frontend/lib/schemas.ts
@@ -5,8 +5,8 @@ const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/web
 
 export const SignUpFormSchema = z.object({
     email: z.string({required_error: "L'email est requis"},).email({ message: "Email invalide" }),
-    password: z.string({required_error: "Le mot de passe est requis"},).min(8),
-    username: z.string({required_error: "Le nom d'utilisateur est requis"},).min(3, "Le nom doit contenir au moins 3 caractères"), 
+    password: z.string({required_error: "Le mot de passe est requis"},).min(8, "Le mot de passe doit contenir au moins 8 caractères"),
+    username: z.string({required_error: "Le nom d'utilisateur est requis"},).min(3, "Le nom d'utilisateur doit contenir au moins 3 caractères"), 
     image: z
     .any()
     .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
