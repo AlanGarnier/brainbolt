d4735db32fe8e683dcde7002d3fae77f281975a2
Samuel NDJOULI AMOUGOU <samuel@kelbongoo.com>
2024-03-19 10:23:46 +0100

    made some modifications in Map Component

--- frontend/components/homepage/sections/map/Map.tsx
+++ frontend/components/homepage/sections/map/Map.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Section from '@/components/Section'
 import Image from 'next/image'
-import { MapSubtitle, MapTitle } from '@/components/CustomTexts'
+import { SectionTitle, TypingText } from '@/components/CustomTexts'
 
 const Map = () => {
   return (
     <Section>
-      <div className="flex flex-col items-center justify-center space-y-8">
-        <MapSubtitle>Lorem ipsum</MapSubtitle>
-        <MapTitle>Lorem ipsum dolor sit amet consectetur. Euismod nam in ut nulla nulla euismod. Proin fermentum mi vitae diam id.</MapTitle>
-        <Image src="/assets/img/map.png" alt="Map" width={1600} height={3200} />
+      <div className="mx-auto flex flex-col text-center items-center justify-center space-y-5 lg:space-y-10 lg:max-w-[763px] mb-9 lg:mb-20">
+        <TypingText>Lorem ipsum</TypingText>
+        <SectionTitle>Lorem ipsum dolor sit amet consectetur. Euismod nam in ut nulla nulla euismod. Proin fermentum mi vitae diam id.</SectionTitle>
       </div>
+      <Image src="/assets/img/map.png" alt="Map" width={1170} height={535} />
     </Section>
   );
 };
