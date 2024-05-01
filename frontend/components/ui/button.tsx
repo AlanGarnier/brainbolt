115681b26e4963ab1556fa128d153b738f8dd1f6
samuel_ndjouli <samuelndj.997@gmail.com>
2024-05-01 14:56:05 +0200

    modified button component

--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -17,6 +17,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
+        white: "border-2 border-white hover:bg-white text-white hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         navigation: "flex items-center justify-center space-x-2 bg-white text-primary-black text-base disabled:cursor-not-allowed disabled:opacity-50 shado-md",
       },
