57dbbbd930e9a802edcdfe7a769355b62de4b2d9
samuel_ndjouli <samuelndj.997@gmail.com>
2024-05-01 11:07:13 +0200

    created ChatCard component

---
+++ frontend/app/dashboard/components/layout/chat/ChatCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ChatCard = () => {
+  return (
+    <div className="hidden md:block md:sticky top-28 right-[28px] bg-primary-black border border-dark-grey rounded-lg p-4 w-96 h-96"
+    >ChatCard</div>
+  )
+}
+
+export default ChatCard
