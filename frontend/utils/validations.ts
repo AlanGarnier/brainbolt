96de0366c914e06c062eab09ac13a8e0451b0fc4
samuel_ndjouli <samuelndj.997@gmail.com>
2024-05-16 16:40:06 +0200

    refactored validations function

--- frontend/utils/validations.ts
+++ frontend/utils/validations.ts
@@ -1,5 +1,5 @@
-export const checkUsernameAvailability = async (pseudo: string) => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/check-username?pseudo=${pseudo}`);
+export const checkCredentialsAvailability = async (field: string, value: string) => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/check-credentials?field=${field}&value=${value}`);
     const data = await res.json();
     return data.isAvailable;
   };
