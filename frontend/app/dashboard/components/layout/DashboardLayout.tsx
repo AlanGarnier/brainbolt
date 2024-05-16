71f208f3808e94d9ae461e03f8b0dc94d6be22fa
samuel_ndjouli <samuelndj.997@gmail.com>
2024-05-16 12:06:26 +0200

    refactored the code in Dashboard Layout

--- frontend/app/dashboard/components/layout/DashboardLayout.tsx
+++ frontend/app/dashboard/components/layout/DashboardLayout.tsx
@@ -4,24 +4,28 @@ import DashboardNav from './navbar/DashboardNav'
 import DashboardSidebar from './sidebar/DashboardSidebar';
 import ChatCard from './chat/ChatCard';
 import DashboardContent from './content/DashboardContent';
+import { User } from '@/lib/types';
 
 interface DashboardLayoutProps {
     children: React.ReactNode
+    user: User
 }
 
-const DashboardLayout: React.FC<DashboardLayoutProps> = ({children}) => {
+const DashboardLayout: React.FC<DashboardLayoutProps> = ({children, user}) => {
   const [open, setOpen] = React.useState(false);
   const handleButtonClick = () => {
     setOpen(!open);
   }
   return (
     <>
-        <DashboardNav />
-        <div className="bg-white dark:bg-primary-black flex overflow-hidden">
-          <DashboardSidebar open={open} handleButtonClick={handleButtonClick} />
-            <DashboardContent>
-              {children}
-            </DashboardContent>
+        <DashboardNav user={user} />
+        <div className="bg-white dark:bg-primary-black flex">
+          <div className="overflow-visible">
+            <DashboardSidebar open={open} handleButtonClick={handleButtonClick} />
+          </div>
+          <DashboardContent>
+            {children}
+          </DashboardContent>
           <ChatCard />
         </div>
     </>
