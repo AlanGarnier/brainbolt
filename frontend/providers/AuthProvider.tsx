50c140175ece29c04744f0da242ad4307c799fab
samuel_ndjouli <samuelndj.997@gmail.com>
2024-05-16 11:57:52 +0200

    modified AuthProvider filename

--- frontend/providers/AuthProvider.tsx
+++ frontend/providers/AuthProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {SessionProvider} from 'next-auth/react'
+import { SessionProvider } from 'next-auth/react'
 
 export interface AuthProviderProps {
   children: React.ReactNode
