fc90823cb5aa4ce014d5365f6a2f459cd2157212
samuel_ndjouli <samuelndj.997@gmail.com>
2024-05-16 12:00:43 +0200

    imported modified filenames in dashboard layout.tsx

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,24 +1,31 @@
 import React, { ReactNode } from 'react'
 import DashboardLayout from './components/layout/DashboardLayout'
 import { ThemeProvider } from '@/providers/ThemeProvider'
+import { getCurrentUser } from '../actions/getCurrentUser'
 
 interface LayoutProps {
     children: ReactNode
 }
 
-const Layout = ({children}: LayoutProps) => {
+const Layout = async ({children}: LayoutProps) => {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <DashboardLayout>
-            {children}
-          </DashboardLayout>
-        </ThemeProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <DashboardLayout user={user}>
+          {children}
+        </DashboardLayout>
+      </ThemeProvider>
     </>
   )
 }
