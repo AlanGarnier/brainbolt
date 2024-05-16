77c0f58059ea1c11f9034de8a0745daab38113cd
samuel_ndjouli <samuelndj.997@gmail.com>
2024-05-16 12:05:11 +0200

    added an if session is true condition

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import LoginForm from './components/LoginForm'
+import getSession from '@/app/actions/get-session'
+import { redirect } from 'next/navigation'
 
-const LoginPage = () => {
+export default async function LoginPage () {
+  const session = await getSession();
+  
+  // If the user is already logged in, redirect to the dashboard
+  if (session) {
+    redirect('/dashboard');
+  }
+
   return (
     <LoginForm />
   )
 }
-
-export default LoginPage
