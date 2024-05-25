4475e99569b6eada3add380acbac4a41ddc35f4e
samuel_ndjouli <samuelndj.997@gmail.com>
2024-05-25 16:24:14 +0200

    imported searchbar component

--- frontend/app/dashboard/components/layout/navbar/DashboardNav.tsx
+++ frontend/app/dashboard/components/layout/navbar/DashboardNav.tsx
@@ -8,6 +8,7 @@ import NotificationsList from './notifications/NotificationsList';
 import { DashboardBottomLinks, DashboardTopLinks } from '@/constants/data';
 import { User } from '@/lib/types';
 import UserMenu from './user/UserMenu';
+import SearchBar from './search/SearchBar';
 
 interface DashboardNavProps {
   user: User;
@@ -51,16 +52,9 @@ const DashboardNav: React.FC<DashboardNavProps> = ({user}) => {
                 className="h-8 mr-3" 
                 alt="Brainbolt Logo" />
             </Link> */}
-            <form action="#" method="GET" className="hidden lg:block lg:ml-[260px]">
-              <label className="sr-only">Search</label>
-              <div className="relative mt-1 lg:w-96">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <Search size={20} className="text-primary-black dark:text-[#BBBBBF]" />
-                  {/* <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg> */}
-                </div>
-                <input type="text" name="search" id="topbar-search" className="bg-white dark:bg-primary-black border font-jost text-white/80 md:text-sm rounded-lg  focus:ring-white block w-full pl-10 p-2.5 border-primary-black/60 dark:border-light-grey placeholder-primary-black/60 dark:placeholder-light-grey focus:outline-none focus:border-primary-black focus:border-[1.5px] dark:focus:border-white" placeholder="Rechercher un jeu/ami..." />
-              </div>
-            </form>
+            <div className="relative inline">
+              <SearchBar />
+            </div>
           </div>
           <div className="flex items-center space-x-2">
             <button id="toggleSidebarMobileSearch" type="button" className="p-2 text-gray-500 rounded-lg lg:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
