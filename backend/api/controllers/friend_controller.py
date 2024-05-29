ee87d010cf32498bf4deab70529cb9ca2ae0fafe
samuel_ndjouli <samuelndj.997@gmail.com>
2024-05-29 22:10:04 +0200

    refactored friend_controller.py , added new routes

--- backend/api/controllers/friend_controller.py
+++ backend/api/controllers/friend_controller.py
@@ -1,46 +1,66 @@
-from flask import request, jsonify
-from flask import Response
-from flask import request
-from bson.json_util import dumps
-from flask import Blueprint
+from flask import request, jsonify, Blueprint
 from ..services.friend_service import FriendService
 
 friend_api = Blueprint("friend", __name__)
 
-
-@friend_api.route('/add_friend', methods=['POST'])
+@friend_api.route('/api/friends/add_friend', methods=['POST'])
 def add_friend():
     user_id = request.json.get('user_id')
     friend_id = request.json.get('friend_id')
 
+    if user_id == friend_id:
+        return jsonify({'error': "You can't send a friend request to yourself"}), 401
+
     if user_id and friend_id:
-        new_friendship = FriendService(user_id, friend_id)
-        new_friendship.save()
-        return {'message': 'Friend request sent successfully'}, 201
+        try:
+            if FriendService.friend_request_exists(user_id, friend_id):
+                return jsonify({'error': 'Friend request already sent'}), 401
+
+            new_friendship = FriendService(user_id, friend_id)
+            new_friendship.save()
+            return jsonify({'message': 'Friend request sent successfully'}), 201
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
     else:
-        return {'error': 'Missing user_id or friend_id'}, 400
+        return jsonify({'error': 'Missing user_id or friend_id'}), 400
 
-
-@friend_api.route('/friends/<user_id>', methods=['GET'])
-def get_friends(user_id):
-    friends = FriendService.get_friends(user_id)
-    return {'friends': friends}, 200
-
-
-@friend_api.route('/pending_requests/<user_id>', methods=['GET'])
+@friend_api.route('/api/friends/pending_requests/<string:user_id>', methods=['GET'])
 def get_pending_requests(user_id):
-    pending_requests = FriendService.get_pending_requests(user_id)
-    return {'pending_requests': pending_requests}, 200
+    if user_id:
+        pending_requests = FriendService.get_pending_requests(user_id)
+        return jsonify({'pending_requests': pending_requests}), 200
+    else:
+        return jsonify({'error': 'Missing user_id'}), 400
 
-
-@friend_api.route('/accept_friend', methods=['POST'])
+@friend_api.route('/api/friends/accept_friend', methods=['POST'])
 def accept_friend():
     user_id = request.json.get('user_id')
     friend_id = request.json.get('friend_id')
 
     if user_id and friend_id:
-        # Update the status of the friend request to 'accepted' for the current user
         FriendService.accept_request(user_id, friend_id)
-        return {'message': 'Friend request accepted successfully'}, 200
+        return jsonify({'message': 'Friend request accepted successfully'}), 200
     else:
-        return {'error': 'Missing user_id or friend_id'}, 400
+        return jsonify({'error': 'Missing user_id or friend_id'}), 400
+
+@friend_api.route('/api/friends/reject_friend', methods=['POST'])
+def reject_friend():
+    user_id = request.json.get('user_id')
+    friend_id = request.json.get('friend_id')
+
+    if user_id and friend_id:
+        FriendService.reject_request(user_id, friend_id)
+        return jsonify({'message': 'Friend request rejected successfully'}), 200
+    else:
+        return jsonify({'error': 'Missing user_id or friend_id'}), 400
+
+@friend_api.route('/api/friends/remove_friend_request', methods=['POST'])
+def remove_friend_request():
+    user_id = request.json.get('user_id')
+    friend_id = request.json.get('friend_id')
+
+    if user_id and friend_id:
+        message, status = FriendService.remove_friend_request(user_id, friend_id)
+        return jsonify(message), status
+    else:
+        return jsonify({'error': 'Missing user_id or friend_id'}), 400
