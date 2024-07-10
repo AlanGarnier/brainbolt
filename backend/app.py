31e4843828dadda3cbc59bd04ce802521018ebe9
samuel_ndjouli <samuelndj.997@gmail.com>
2024-07-10 14:31:04 +0200

    added socket cors

--- backend/app.py
+++ backend/app.py
@@ -18,6 +18,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['MONGO_URI'] = os.getenv('MONGO_DB_CONN_STRING')
 # CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 # Set all routes
 # app.register_blueprint(game_api)
 app.register_blueprint(user_api)
@@ -33,8 +34,6 @@ app.config["JWT_SECRET_KEY"] = AuthConfig.jwt_secret_key
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = AuthConfig.jwt_access_token_expires
 app.config["JWT_REFRESH_TOKEN_EXPIRES"] = AuthConfig.jwt_refresh_token_expires
 jwt = JWTManager(app)
-
-socketio = SocketIO(app)
 
 @app.route("/health", methods=["GET"])
 def check_api_health():
