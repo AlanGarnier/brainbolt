f5d1143f06de74ba30fb5a2bb969a5e8a82e21f9
samuel_ndjouli <samuelndj.997@gmail.com>
2024-07-11 17:29:36 +0200

    fixed the code

--- frontend/app/dashboard/profile/[userId]/page.tsx
+++ frontend/app/dashboard/profile/[userId]/page.tsx
@@ -12,28 +12,28 @@ const ProfilePage = async () => {
     return (
         <>
             <div className="w-11/12 ">
-            <DashboardTitle>Mon Profil 🎮 </DashboardTitle>   
-        </div>
-        <div className="flex flex-col items-center justify-center  mt-4">
-            <div className=" p-8 rounded-lg text-center">
-                <div className="relative rounded-full overflow-hidden w-32 h-32 mb-4">
-                    <Image
-                        src={user?.picture || "/assets/img/user-placeholder.png"}
-                        alt={user?.pseudo || "User"}
-                        layout="fill"
-                        className="object-cover"
-                    />
-                </div>
-                <DashboardTitle>{user.pseudo}</DashboardTitle>
+                <DashboardTitle>Mon Profil 🎮 </DashboardTitle>   
             </div>
-            <div className="text-center">
-                        <div className="flex flex-col items-center justify-center gap-8">
-                             <DashboardSubtitleScore>Score</DashboardSubtitleScore>
-
-                            <DashboardText> {"user?.score" || "Vous n'avez toujours pas de score á afficher"}</DashboardText>
+            <div className="flex flex-col items-center justify-center  mt-4">
+                <div className=" p-8 rounded-lg text-center">
+                    <div className="relative rounded-full overflow-hidden w-32 h-32 mb-4">
+                        <Image
+                            src={user?.picture || "/assets/img/user-placeholder.png"}
+                            alt={user?.pseudo || "User"}
+                            layout="fill"
+                            className="object-cover"
+                        />
+                    </div>
+                    <DashboardTitle>{user.pseudo}</DashboardTitle>
+                </div>
+                <div className="text-center">
+                    <div className="flex flex-col items-center justify-center gap-8">
+                        <div className="flex space-x-4">
+                            <DashboardSubtitleScore>Nombre de victoires :</DashboardSubtitleScore>
                         </div>
-             </div>
-        </div>
+                    </div>
+                </div>
+            </div>
         </>  
 
     );
--- frontend/app/dashboard/user/[userId]/page.tsx
+++ frontend/app/dashboard/user/[userId]/page.tsx
@@ -4,10 +4,12 @@ import { DashboardSubtitleScore, DashboardTitle,DashboardText } from "@/componen
 import Image from "next/image";
 import { ProfileButton } from "@/components/ui/profilebutton";
 import Link from 'next/link'
+import { getUser } from "@/app/actions/getUser";
 
 
-const UserProfilePage = async () => {
-    const user = await getCurrentUser();
+const UserProfilePage = async ({params}: {params: {userId: string} }) => {
+    const { userId } = params;
+    const user = await getUser(userId);
     if (!user) {
         return null;
     }
@@ -15,38 +17,40 @@ const UserProfilePage = async () => {
     return (
         <>
             <div className="w-11/12 ">
-            <DashboardTitle>Profil joueur 🎮</DashboardTitle>   
-        </div>
-        <div className="flex flex-col items-center justify-center  mt-4">
-            <div className=" p-8 rounded-lg text-center">
-                <div className="relative rounded-full overflow-hidden w-32 h-32 mb-4">
-                    <Image
-                        src={user?.picture || "/assets/img/user-placeholder.png"}
-                        alt={user?.pseudo || "User"}
-                        layout="fill"
-                        className="object-cover"
-                    />
-                </div>
-                <DashboardTitle>{user.pseudo}</DashboardTitle>
-
+                <DashboardTitle>Profil joueur 🎮</DashboardTitle>   
             </div>
-            <div className="text-center">
-                        <div className="flex flex-col items-center justify-center gap-8">
-                        <ProfileButton
-                        asChild
-                        variant={'default'}
-                        className="glow">
-                            <Link
-                                href="#"
-                                >
-                                Ajouter en ami 
-                            </Link>
-                    </ProfileButton>                             <DashboardSubtitleScore>Score</DashboardSubtitleScore>
+            <div className="flex flex-col items-center justify-center  mt-4">
+                <div className=" p-8 rounded-lg text-center">
+                    <div className="relative mx-auto rounded-full overflow-hidden w-32 h-32 mb-4">
+                        <Image
+                            src={user?.picture || "/assets/img/user-placeholder.png"}
+                            alt={user?.pseudo || "User"}
+                            layout="fill"
+                            className="object-cover"
+                        />
+                    </div>
+                    <DashboardTitle>{user.pseudo}</DashboardTitle>
 
-                            <DashboardText> {"user?.score" || "Votre ami n'a toujours pas de score á afficher"}</DashboardText>
+                </div>
+                <div className="text-center">
+                    <div className="flex flex-col items-center justify-center gap-8">
+                        {/* <ProfileButton
+                            asChild
+                            variant={'default'}
+                            className="glow">
+                                <Link
+                                    href="#"
+                                    >
+                                    Ajouter en ami 
+                                </Link>
+                        </ProfileButton> */}
+                        <div className="flex space-x-4">
+                            <DashboardSubtitleScore>Nombre de victoires :</DashboardSubtitleScore>
                         </div>
-             </div>
-        </div>
+                        
+                    </div>
+                </div>
+            </div>
         </>  
 
     );
